99f0e7e42bffa155223c72cfd9c56c679224bdd8
Misty Blue <hellomistyblue@gmail.com>
2025-07-25 16:32:24 -0500

    added files

---
+++ scripts/database.js
@@ -0,0 +1,29 @@
+export const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ]
+    villains: [
+        {
+            id: 1
+            name: "CircuitBreaker"
+            power: "Destroys electronics"
+        }
+        {
+            id: 2 
+            name: "Malware Maven"
+            power: "Mind Control"
+        }
+    ]
+}
+export const getVillains = () => {
+    return structuredClone(database.villains);
+}
